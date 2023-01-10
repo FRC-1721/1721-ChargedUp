116371a2339bab9b9d88a3daf6698f20aeccd2c1
Jack Jewett <jewett818@gmail.com>
2023-01-10 13:44:03 -0500

    I looked through the snap command in the discord bot to find what we use and luckily it was also cv2 so that makes it easier. Next I will get onto error handling because there will be a lot ofpotential errors and we don't want the robot to crash everytime the cam doesn't connect properly. also we can maybe try the code today during robtics?

--- rio/commands/detectTag.py
+++ rio/commands/detectTag.py
@@ -1,21 +1,23 @@
 import apriltag
-import cv2
+import cv2 as cv
 import commands2
 
 # is this where this file should go?
-class detectTag(commands2.CommandBase):  #
+class detectTag(commands2.CommandBase):
     def __init__(self) -> None:
         super().__init__()
         # stuff will go here
 
-    def getCapture():
-        pass
-        # will maybe be used to get the capture s it can be used in tagInfo()
+    def getCapture(self):
+        cam = cv.VideoCapture(0)
+        capture = cam.read()
+        return capture
 
-    def tagInfo(image):
-        img = cv2.imread(
-            image, cv2.IMREAD_GRAYSCALE
+    def tagInfo(self):
+        image = self.getCapture()
+        img = cv.imread(
+            image, cv.IMREAD_GRAYSCALE  # NEEDS TO BE GRAYSCALED VIA THIS COMMAND
         )  # image is your import, I'm not sure how to get the video on the robot, but i'm guessing a snapshot should be taken at set intervals and fed into image
-        detector = apriltag.Detector()
-        result = detector.detect(img)
+        detector = apriltag.Detector()  # this is the object that detetcts the tag
+        result = detector.detect(img)  # this is the method that does the detection
         return result
