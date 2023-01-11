12509f856a82d3f66539686e6098af477872574e
Jack Jewett <jewett818@gmail.com>
2023-01-11 16:06:32 -0500

    fixed some stuff. also Joe if i run this command with the cam plugged into the raspberry pi will it still work just as well?

--- rio/commands/detectTag.py
+++ rio/commands/detectTag.py
@@ -6,17 +6,12 @@ import commands2
 class detectTag(commands2.CommandBase):
     def __init__(self) -> None:
         super().__init__()
-        # stuff will go here
-
-    def getCapture(self):
-        cam = cv.VideoCapture(0)
-        capture = cam.read()
-        return capture
+        self.cam = cv.VideoCapture(0)
 
     def tagInfo(self):
-        image = self.getCapture()
+        capture = self.cam.read()
         img = cv.imread(
-            image, cv.IMREAD_GRAYSCALE  # NEEDS TO BE GRAYSCALED VIA THIS COMMAND
+            capture, cv.IMREAD_GRAYSCALE  # NEEDS TO BE GRAYSCALED VIA THIS COMMAND
         )  # image is your import, I'm not sure how to get the video on the robot, but i'm guessing a snapshot should be taken at set intervals and fed into image
         detector = apriltag.Detector()  # this is the object that detetcts the tag
         result = detector.detect(img)  # this is the method that does the detection
