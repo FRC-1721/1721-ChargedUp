d6285303b9e6a864f648074a556a8471eba41a5e
kredcool <kredcool2007@outlook.com>
2023-01-22 14:49:18 -0500

    the PID values will need to be updated once we get the bot

--- rio/subsystems/clawsubsystem.py
+++ rio/subsystems/clawsubsystem.py
@@ -4,7 +4,6 @@
 import wpilib
 import wpilib.drive
 import commands2
-import math
 
 # Constants
 from constants.constants import getConstants
@@ -26,49 +25,22 @@ class ArmSubsystem(commands2.SubsystemBase):
         self.clawConst = constants["claw"]
         self.pidConst = pidConstants["claw"]
 
-        # The motor on the back of the drive. (The arm extension motor)
-        self.backMotor = CANSparkMax(
-            self.backCosnt["MotorPort"],
+        # the claw motor
+        self.motor = CANSparkMax(
+            self.clawConst["MotorPort"],
             CANSparkMaxLowLevel.MotorType.kBrushless,
         )
 
-        self.backMotors.setInverted(self.backCosnt["Inverted"])
+        self.motor.setInverted(self.clawConst["Inverted"])
 
-        # The motor on the middle of the drive. (The screw motor)
-        self.middleMotor = CANSparkMax(
-            self.middleCosnt["MotorPort"],
-            CANSparkMaxLowLevel.MotorType.kBrushless,
+        # TODO: These should to be replaced with CAN motor controllers
+        self.encoder = wpilib.Encoder(
+            self.clawConst["EncoderPorts"],
+            self.clawConst["EncoderReversed"],
         )
-
-        self.middleMotors.setInverted(self.middleCosnt["Inverted"])
-
-        # TODO: Replace with proper motorconfigs
-        self.backMotor.setInverted(False)
-        self.middleMotor.setInverted(False)
-
-        # TODO: These need to be replaced with CAN motor controllers
-        # The back-side arm encoder
-        self.backEncoder = wpilib.Encoder(
-            self.backCosnt["EncoderPorts"],
-            self.backCosnt["EncoderReversed"],
-        )
-
-        # The middle-side arm encoder
-        self.middleEncoder = wpilib.Encoder(
-            self.middleCosnt["EncoderPorts"],
-            self.middleCosnt["EncoderReversed"],
-        )
-
-        # Sets the distance per pulse for the encoders
-        encoderDistPerP = (
-            self.armConst["kWheelDiameterInches"] * math.pi
-        ) / self.armConst["kEncoderCPR"]
-
-        self.backEncoder.setDistancePerPulse(encoderDistPerP)
-        self.middleEncoder.setDistancePerPulse(encoderDistPerP)
 
         # Pid values
-        self.PID = self.backMotor.getPIDController()
+        self.PID = self.motor.getPIDController()
         self.PID.setP(self.pidConst["kp"])
         self.PID.setI(self.pidConst["ki"])
         self.PID.setD(self.pidConst["kd"])
