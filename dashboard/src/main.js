4d9fe18349fa5f1fe2994573664fce1cc52c5f03
dublUayaychtee <mail@dublu.net>
2023-01-26 17:24:00 -0500

    added distractions

--- dashboard/src/main.js
+++ dashboard/src/main.js
@@ -95,25 +95,66 @@ function updateColors() {
 
 // p5.js canvas
 
+function getRandomInt (min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
-let img;
+let img, joe, dvd;
+
+let bounceJoe = [Math.random() < 0.5, Math.random() < 0.5, getRandomInt(0,753), getRandomInt(0,341)];
+let bounceDvd = [Math.random() < 0.5, Math.random() < 0.5, getRandomInt(0,681), getRandomInt(0,328)];
+let running = false;
 
 function preload() {
 	img = loadImage(require('./assets/field.png'));
+	joe = loadImage(require('./assets/joe.png'));
+	dvd = loadImage(require('./assets/dvd.png'));
 }
 
 function setup() {
     let canvas = createCanvas(777, 377);
-    canvas.parent("info-three");
+    canvas.parent("info-two");
 	frameRate(24);
 }
 
 function draw() {
     background(220);
 	image(img, 0, 0);
+	image(dvd, bounceDvd[2], bounceDvd[3]);
+	image(joe, bounceJoe[2], bounceJoe[3]);
+	if (running) {
+		bounceJoe[2] += bounceJoe[0] ? 3 : -3;
+		bounceJoe[3] += bounceJoe[1] ? 3 : -3;
+		bounceDvd[2] += bounceDvd[0] ? 4 : -4;
+		bounceDvd[3] += bounceDvd[1] ? 4 : -4;
+		rect(5, 5, 5, 20);
+		rect(20, 5, 5, 20);
+	} else {
+		triangle(5, 5, 25, 15, 5, 25);
+	}
+	if (bounceJoe[2] < 0 || bounceJoe[2] > 753) {
+		bounceJoe[0] = !bounceJoe[0];
+	}
+	if (bounceJoe[3] < 0 || bounceJoe[3] > 341) {
+		bounceJoe[1] = !bounceJoe[1];
+	}
+	if (bounceDvd[2] < 0 || bounceDvd[2] > 681) {96
+		bounceDvd[0] = !bounceDvd[0];
+	}
+	if (bounceDvd[3] < 0 || bounceDvd[3] > 328) {49
+		bounceDvd[1] = !bounceDvd[1];
+	}
+}
+
+
+function mousePressed() {
+	if (5 < mouseX < 25 && 5 < mouseY < 25) {
+		running = !running;
+	}
 }
 
 window.preload = preload;
 window.setup = setup; 
 window.draw = draw;
+window.mousePressed = mousePressed;
 //window.keyPressed = keyPressed;
