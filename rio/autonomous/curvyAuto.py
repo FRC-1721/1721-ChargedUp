32a7d10db1cf2cb46eeed1498ce1b559814f8a92
kredcool <kredcool2007@outlook.com>
2023-02-08 17:23:41 -0500

    I have a feeling I will be stuck here for a bit, it is erroring in super init

---
+++ rio/autonomous/curvyAuto.py
@@ -0,0 +1,29 @@
+# commands2
+import commands2
+
+from commands2 import WaitCommand
+
+# commands and autonomous
+from autonomous.crossLinePath import CrossLinePath
+from commands.extend import Extend
+from commands.retract import Retract
+from commands.unclamp import Unclamp
+
+
+class CurvyAuto(commands2.SequentialCommandGroup):
+    def __init_(
+        self,
+    ) -> None:
+        """
+        This is the auto that leaves the comunity
+        and goes back onto the switch
+        """
+        super().__init__(
+            Extend()
+            WaitCommand(2)
+            Unclamp()
+            WaitCommand(2)
+            Retract()
+            WaitCommand(2)
+            CrossLinePath(2)
+        )
