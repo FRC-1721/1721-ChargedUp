df4a74ddedae4fd3c8643ab4bdc28737cd5a135e
Jack Jewett <jewett818@gmail.com>
2023-02-13 10:24:11 -0500

    added a function which takes tag id and position and uses them to update the transform values of an fmap file. this function will prove better than getpos() during testing but in comp it's not the most useful due to getpos existing

--- rio/commands/limelight.py
+++ rio/commands/limelight.py
@@ -1,19 +1,21 @@
 from networktables import NetworkTables as NT
 import wpilib
 import commands2
+import json
 
 
 class limeLight(commands2.Command):
     def __init__(self) -> None:
-        self.tables = NT.getTable("limelight")  # info we recieve from the limelight
-        self.map = map = open("/frc2023.fmap", "r")  # fmap file
+        self.table = NT.getTable("limelight")  # info we recieve from the limelight
+        self.map = json.load(open("/frc2023.fmap", "r"))  # fmap file
 
     def getTag(self):
-        Id = "No Id found"
-        tagID = self.tables.getEntry("tid").getString(Id)
-        return tagID
+        """Tries to get any apriltag in view of limelight. returns 0 if no apriltag is found."""
+        tagID = self.table.getEntry("tid").getDouble(0)
+        tagPos = self.table.getEntry("targetpose_robotspace").getDoubleArray(0)
+        self.map["fiducials"][tagID - 1]["transform"] = tagPos
+        return tagPos
 
     def getBotPos(self):
-        Pos = "No Id Found"
-        tagID = self.tables.getEntry("botpose_targetspace").getString(Pos)
-        return tagID
+        pos = self.table.getEntry("botpose_targetspace").getDoubleArray(0)
+        return pos
