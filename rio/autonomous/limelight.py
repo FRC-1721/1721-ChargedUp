c175c82f416b5e080dd3d8a626df4c9922a8f5f2
KenwoodFox <kenwood364@gmail.com>
2023-02-14 10:29:21 -0500

    Fix Jack's code

--- rio/autonomous/limelight.py
+++ rio/autonomous/limelight.py
@@ -1,16 +1,24 @@
 from ntcore import NetworkTableInstance
-import wpilib
+
+from wpilib import RobotBase
+
 import commands2
 import json
 
 
-class limeLightCommands(commands2.Command):
+class limeLightCommands(commands2.CommandBase):
     def __init__(self) -> None:
-        self.binger = NetworkTableInstance.getDefault()
-        self.table = self.binger.getTable(
-            "limelight"
-        )  # info we recieve from the limelight
-        self.map = json.load(open("autonomous/frc2023.fmap", "r"))  # fmap file
+        super().__init__()
+        # Configure networktables
+        self.nt = NetworkTableInstance.getDefault()
+        self.ll = self.nt.getTable("limelight")
+
+        # Load map
+        mapPath = "deploy/maps/frc2023.fmap"
+        if RobotBase.isReal():
+            mapPath = "/home/lvuser/py/deploy/maps/frc2023.fmap"
+
+        self.map = json.load(open(mapPath, "r"))  # Load fmap file
 
     def getTag(self):
         """Tries to get any apriltag in view of limelight. returns 0 if no apriltag is found."""
