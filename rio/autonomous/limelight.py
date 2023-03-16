0377c41fc322633055f81fc9106ae9625df83131
Jack Jewett <jewett818@gmail.com>
2023-03-16 12:19:07 -0400

    -Added a feature which should let the user select an aprilTag ID to focus
    -Added comments to make code more understandable and cohesive
    
    -got rid of unnecessary/broken functions

--- rio/autonomous/limelight.py
+++ rio/autonomous/limelight.py
@@ -11,13 +11,14 @@ import json
 
 
 class limeLightCommands(commands2.CommandBase):
-    def __init__(self, drive: DriveSubsystem) -> None:
+    def __init__(self, drive: DriveSubsystem, TagID=None) -> None:
         super().__init__()
         # Configure networktables
         self.nt = NetworkTableInstance.getDefault()
         self.ll = self.nt.getTable("limelight-gabriel")
         self.botPos = 0
         self.drivesys = drive
+        self.targetID = TagID
         # Load map
         mapPath = "deploy/maps/frc2023.fmap"
         if RobotBase.isReal():
@@ -26,37 +27,50 @@ class limeLightCommands(commands2.CommandBase):
         # self.map = json.load(open(mapPath, "r"))  # Load fmap file
 
     def execute(self) -> None:
-        aprilTag = self.getTag()
-        if type(aprilTag) is list and len(aprilTag) >= 6:
+        aprilTag = (
+            self.getTag()
+        )  # uses nt tables to try to retreive info about an apriltag
+        if (
+            type(aprilTag) is list and len(aprilTag) >= 6
+        ):  # checks tif the apriltag is a list and has the correct amount of values
             print(aprilTag, "asdfasdfasdf")
-            distX = aprilTag[0]
-            distY = aprilTag[1]
-            rotZ = aprilTag[5]
+            distX = aprilTag[0]  # x linear offset of apriltag and robot
+            distY = aprilTag[1]  # y linear offset of apriltag and robot
+            rotZ = aprilTag[5]  # z rotational offset of apriltag and robot
+
+            # due to the camera not being able to capture the tag in a
+            # stable manner as well as how it handles rotation, this
+            # checks whether the z rot offset is negative or positive
+            # and converts it into a better value
             if rotZ > 0:
                 rotZ -= 180
             else:
                 rotZ += 180
 
+            # sets a default speed and rotational speed value
             movespeed = 0
             rotSpeed = 0
 
             print("Z: " + str(rotZ))
             print("Y: " + str(distY))
-            if -5 < rotZ < 5:
-                if distX >= -4:
-                    movespeed = 1  # needs to be adjusted
 
+            # checks the angle offset of the robot and decides whether it should go straight forward or start turning
+            if -5 < rotZ < 5:
+                # checks how far away from the apriltag the robot is and sets speed accordingly
+                if distX >= -4:
+                    movespeed = 0.8  # needs to be adjusted
                 elif distX >= -5:
-                    movespeed = 0.7
+                    movespeed = 0.6
                 elif distX >= -6:
                     movespeed = 0.4
 
             elif -20 < rotZ < 20:
-                if rotZ > 1:
+                if rotZ > 0:
                     rotSpeed = 0.25
-                elif rotZ < 20:
+                elif rotZ < 0:
                     rotSpeed = -0.25
             print("movespeed: " + str(movespeed))
+            # drive the robot based on values
             self.drivesys.arcadeDrive(movespeed, rotSpeed)
         else:
             if aprilTag <= 0:
@@ -69,20 +83,14 @@ class limeLightCommands(commands2.CommandBase):
         Tries to get any apriltag in view of limelight. returns 0 if no apriltag is found.
         If A Number is passed into ID, The limelight will only look for apriltags of that ID
         """
-
+        # gets the tagID from ntables
         tagID = self.ll.getEntry("tid").getDouble(0)
-        print(tagID, "a")
-        if 0 < tagID <= 8:
-            tagPos = self.ll.getEntry("botpose").getDoubleArray(1)
-            return tagPos
-        # self.map["fiducials"][tagID - 1]["transform"] = tagPos
-        return tagID
 
-    def getBotPos(self):
-        pos = self.ll.getEntry("botpose_targetspace").getType()
-        return pos
-
-    def autoBotDrive(self):
-        self.botPos = self.getBotPos()
-        if self.botPos != 0:
-            TurnToAngle(6, self.drivesys)
+        # checks if targetID has been set and if it has, checks to make sure that it has the right ID
+        if self.targetID == None ^ tagID == self.targetID:
+            print(tagID, "a")
+            if 0 < tagID <= 8:
+                tagPos = self.ll.getEntry("botpose").getDoubleArray(1)
+                return tagPos
+            # self.map["fiducials"][tagID - 1]["transform"] = tagPos
+            return tagID
