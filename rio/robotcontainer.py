1f381206594ce648ede68b97be30b3f8b1451769
kredcool <kredcool2007@outlook.com>
2023-03-18 15:07:59 -0400

    slowing code

--- rio/robotcontainer.py
+++ rio/robotcontainer.py
@@ -25,13 +25,14 @@ from commands.presetArm import PresetArm
 from commands.manualArm import ManualArm
 from commands.findZero import FindZero
 from commands.holdPosition import HoldPosition
+from commands.flyforwire import FlyForWire
 
 # Autonomous
 from autonomous.noauto import NoAuto
-from autonomous.backAuto import BackAuto
-from autonomous.dropAuto import DropAuto
 from autonomous.dropDriveAuto import DropDriveAuto
 from autonomous.blockDrop import BlockDrop
+from autonomous.longBlockDrop import LongBlockDrop
+from autonomous.longDropDriveAuto import LongDropDrive
 
 # NetworkTables
 from ntcore import NetworkTableInstance
@@ -223,6 +224,21 @@ class RobotContainer:
             self.operatorConsts["hold"],
         ).toggleOnTrue(ManualGripper(self.clawSubsystem, grabForce=-0.35))
 
+        # a slow command
+        commands2.button.JoystickButton(
+            self.driverController, self.driverConsts["Slow"]
+        ).whileTrue(
+            FlyForWire(
+                self.robotDrive,
+                lambda: -self.driverController.getRawAxis(
+                    self.driverConsts["ForwardAxis"],
+                ),
+                lambda: self.driverController.getRawAxis(
+                    self.driverConsts["SteerAxis"],
+                ),
+            )
+        )
+
     def configureAutonomous(self):
         # Create a sendable chooser
         self.autoChooser = wpilib.SendableChooser()
@@ -230,11 +246,18 @@ class RobotContainer:
         # Add options for chooser
         self.autoChooser.setDefaultOption("No Auto", NoAuto())
         self.autoChooser.addOption(
-            "Cone Drop Auto", DropDriveAuto(self.armSubsystem, self.robotDrive)
+            "Short Cone Drop", DropDriveAuto(self.armSubsystem, self.robotDrive)
         )
         self.autoChooser.addOption(
-            "Block Drop Auto",
+            "Long Cone Drop", LongDropDrive(self.armSubsystem, self.robotDrive)
+        )
+        self.autoChooser.addOption(
+            "Short Block Drop",
             BlockDrop(self.clawSubsystem, self.armSubsystem, self.robotDrive),
+        )
+        self.autoChooser.addOption(
+            "Long Block Drop",
+            LongBlockDrop(self.clawSubsystem, self.armSubsystem, self.robotDrive),
         )
 
         # Put the chooser on the dashboard
